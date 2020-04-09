f486e9379fb51642e3c2b05a5d320d95510c5cf2
James <JamesShoaf@github.com>
2020-04-08 18:15:06 -0700

    implement randomImage generators, plan out dataGenerator

--- dataGenerator.js
+++ dataGenerator.js
@@ -6,10 +6,35 @@ const randomInt = (max, min = 0) => {
 
 const randomUserImage = () => {
   const images = [
-    'url1', 'url2', 'url3', 'url4', 'url5', 'url6', 'url7', 'url8', 'url9', 'url10'
-  ]
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/377.png",
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/378.png",
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/379.png",
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/380.png",
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/381.png",
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/382.png",
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/383.png",
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/384.png",
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/385.png",
+    "https://yolk-io-images.s3-us-west-1.amazonaws.com/avatars/386.png"
+  ];
   return images[randomInt(10)];
-}
+};
+
+const randomRoomImage = () => {
+  const images = [
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/1.jpg',
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/2.jpg',
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/3.jpg',
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/4.jpg',
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/5.jpg',
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/6.jpg',
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/7.jpg',
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/8.jpg',
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/9.jpg',
+    'https://yolk-io-images1.s3-us-west-1.amazonaws.com/rooms/10.jpg',
+  ];
+  return images[randomInt(10)];
+};
 
 const generateUser = (id) => {
   const isHost = !Boolean(id % 2);
@@ -20,4 +45,32 @@ const generateUser = (id) => {
     is_super_host: superHost,
     profile_image_url: `${randomUserImage()}`,
   };
+};
+
+const dataGenerator = (primaryRecordCount) => {
+  var roomID = 1;
+  const loopSize = 250000;
+  var loops = 1;
+  var userID =1;
+  var imageID = 1;
+  var reviewID = 1;
+  var dateID = 1;
+
+  while (roomID < primaryRecordCount) {
+    for (let i = 0; i < loopSize; i += 1) {
+      const user = generateUser(userID);
+      if (!Boolean(userID %2)) {
+        const randomRoom = randomInt(9, 1);
+        for (let j = 0; j < randomRoom; j += 1) {
+          const room = generateRoom(roomID, userID);
+          roomID += 1;
+          //write room to CSV
+        }
+      }
+      userID += 1;
+      //write user to CSV
+    }
+    console.log(`Finished loop ${loops}: ${userID} users, ${roomID} rooms`)
+  }
+  console.log(`Done! Finished ${loops} loops: ${userID} users, ${roomID} rooms`);
 };
