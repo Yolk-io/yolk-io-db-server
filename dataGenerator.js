dfae198cc5b45eb00c5fa95f21b1a55e64d9da0c
James <james.s.shoaf@gmail.com>
2020-04-14 20:08:31 -0700

    use await for each data type - 67k rooms, 95s

--- dataGenerator.js
+++ dataGenerator.js
@@ -8,9 +8,9 @@ const {
   userPipeGenerator,
   roomPipeGenerator,
   imagePipeGenerator,
+  rulesPipeGenerator,
   datePipeGenerator,
   reviewPipeGenerator,
-  rulesPipeGenerator,
 } = require('./helpers/streamWriters');
 
 //randomizers
@@ -19,7 +19,7 @@ const {
 } = require('./helpers/randomizers');
 
 const dataGenerator = async (primaryRecordCount) => {
-  const loopSize = 100000;
+  const loopSize = 10000;
   
   const startTime = new Date();
   const dateArray = generateDateArray(); //next 93 days
@@ -31,46 +31,49 @@ const dataGenerator = async (primaryRecordCount) => {
   let reviewID = 1;
 
   
-  // for (; roomID < primaryRecordCount; loops += 1) {
+  for (; roomID < primaryRecordCount; loops += 1) {
     // use below instead for User-only tests
-  for (; userID < primaryRecordCount; loops += 1) {
-    const userWriteStream = saveFile('users', loops);
-/*     const roomWriteStream = saveFile('rooms', loops);
-    const imageWriteStream = saveFile('images', loops);
-    const dateWriteStream = saveFile('dates', loops);
-    const rulesWriteStream = saveFile('rules', loops); */
-    const [userReadStream, writeUser] = userPipeGenerator(userWriteStream);
-/*     const writeRoom = roomPipeGenerator(roomWriteStream);
-    const writeImage = imagePipeGenerator(imageWriteStream);
-    const writeDate = datePipeGenerator(dateWriteStream);
-    const writeRules = rulesPipeGenerator(rulesWriteStream); */
+  // for (; userID < primaryRecordCount; loops += 1) {
+    const [userReadStream, writeUser] = userPipeGenerator(saveFile('users', loops));
+    const [roomReadStream, writeRoom] = roomPipeGenerator(saveFile('rooms', loops));
+    const [imageReadStream, writeImage] = imagePipeGenerator(saveFile('images', loops));
+    const [rulesReadStream, writeRules] = rulesPipeGenerator(saveFile('rules', loops));
+    const [dateReadStream, writeDate] = datePipeGenerator(saveFile('dates', loops));
+
     for (let i = 0; i < loopSize; i += 1, userID += 1) {
-      const userWriteBool = writeUser(userID);
-      if(!userWriteBool) {
+      if(!writeUser(userID)) {
         await new Promise((resolve) => userReadStream.once('drain', resolve));
       }
 
-      /* if (Boolean(randomInt(2) === 0)) { //user is a host
+      if (Boolean(randomInt(2) === 0)) { //user is a host
         const roomsHosted = randomInt(9, 1);
         for (let j = 0; j < roomsHosted; j += 1, roomID += 1) {
           
-          data.push(writeRoom(roomID, userID));
+          if(!writeRoom(roomID, userID)) {
+            await new Promise((resolve) => roomReadStream.once('drain', resolve));
+          }
           
           const yolkVerified = Boolean(randomInt(2) === 1);
           const imageCount = randomInt(5);
 
           for (let k = 0; k < imageCount; k += 1, imageID += 1) {
-            data.push(writeImage(imageID, roomID, yolkVerified));
+            if(!writeImage(imageID, roomID, yolkVerified)){
+              await new Promise((resolve) => imageReadStream.once('drain', resolve));
+            }
           }
 
           const basePrice = randomInt(300, 20);
-          data.push(writeRules(roomID, basePrice));
+          if(!writeRules(roomID, basePrice)) {
+            await new Promise((resolve) => rulesReadStream.once('drain', resolve));
+          }
           
           for (let k = 0; k < dateArray.length; k += 1) {
-            data.push(writeDate(roomID, dateArray[k], basePrice));
+            if(!writeDate(roomID, dateArray[k], basePrice)) {
+              await new Promise((resolve) => dateReadStream.once('drain', resolve));
+            }
           }
         }
-      } */
+      }
     }
     console.log(`Finished primary loop ${loops}: ${userID - 1} users, ${roomID - 1} rooms: ${new Date() - startTime} ms`);
   }
@@ -78,28 +81,27 @@ const dataGenerator = async (primaryRecordCount) => {
   
   // // generate reviews
   // // choose relative number of reviews here
-/*   const avgReviewsPerRoom = 5;
+  const avgReviewsPerRoom = 5;
   // restart loop count for file naming purposes
   loops = 1;
 
   // generate at least (avg * rooms) reviews
   for (; reviewID < roomID * avgReviewsPerRoom; loops += 1) {
     // open file stream
-    const reviewWriteStream = saveFile('reviews', loops);
-    const writeReview = reviewPipeGenerator(reviewWriteStream);
-    const data = [];
+    const [reviewReadStream, writeReview] = reviewPipeGenerator(saveFile('reviews', loops));
     for (let i = 0; i < loopSize; i += 1, reviewID += 1) {
       const randomRoomID = randomInt(roomID + 1, 1);
       const randomGuest = randomInt(userID + 1, 1);
-      data.push(writeReview(reviewID, randomRoomID, randomGuest));
+      if(!writeReview(reviewID, randomRoomID, randomGuest)) {
+        await new Promise((resolve) => reviewReadStream.once('drain', resolve));
+      }
     }
-    await Promise.all(data);
     console.log(`Finished secondary loop ${loops}: generated ${reviewID - 1} reviews: ${new Date() - startTime} ms`);
   }
-  console.log(`Secondary Records Done! Finished ${loops -1} loops and generated ${reviewID - 1} reviews: ${new Date() - startTime} ms`); */
+  console.log(`Secondary Records Done! Finished ${loops -1} loops and generated ${reviewID - 1} reviews: ${new Date() - startTime} ms`);
   console.log(`Final results: ${roomID - 1} rooms, ${userID -1} users, ${reviewID - 1} reviews: ${new Date() - startTime} ms`);
 };
 
-dataGenerator(10000000);
+dataGenerator(50000);
 
 module.exports = dataGenerator;
