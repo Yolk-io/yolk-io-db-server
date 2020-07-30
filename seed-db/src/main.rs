afd007844c8f2561d9b423e01ad5e251427753b0
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-07-29 20:06:28 -0700

    add User struct

--- seed-db/src/main.rs
+++ seed-db/src/main.rs
@@ -1,3 +1,10 @@
+struct User {
+    id: u32,
+    username: String,
+    is_super_host: bool,
+    profile_image: String
+}
+
 fn main() {
     println!("What if we used Rust instead of Node to seed the db?");
 }
